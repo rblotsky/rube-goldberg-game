7ddde2621cc38e49a117fd90c0909e7dbd5fd2f7
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2022-04-30 16:45:30 -0400

    added some comments

--- Assets/Source/Menu/GlobalData.cs
+++ Assets/Source/Menu/GlobalData.cs
@@ -5,6 +5,9 @@ namespace RuthGoldbergGame
     public static class GlobalData
     {
         static List<LevelData>levelsInGame = new List<LevelData>();
-        
+        //looking online people say scriptable objects are the best way to store data for levels
+        //maybe we should have
+        //TODO: create a function to load a level from json file
+        //have all files in a single file, and unload all things from that file
     }
 }
