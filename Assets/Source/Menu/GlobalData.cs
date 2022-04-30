5499025f6c7be3413db00044750f106414df84cd
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2022-04-30 16:54:58 -0400

    added more comments and suggestions

--- Assets/Source/Menu/GlobalData.cs
+++ Assets/Source/Menu/GlobalData.cs
@@ -7,7 +7,12 @@ namespace RuthGoldbergGame
         static List<LevelData>levelsInGame = new List<LevelData>();
         //looking online people say scriptable objects are the best way to store data for levels
         //maybe we should have
-        //TODO: create a function to load a level from json file
-        //have all files in a single file, and unload all things from that file
+        //TODO: populating levelsInGame at runtime
+        //maybe have all files in a single JSON file, and unload all things from that file with a function
+        
+        /* static void ChangeCompletionOnLevel(int id, Completion newCompl)
+        {
+            levelsInGame.Find(x => x.LevelID == id).IsBeat = newCompl;
+        }*/
     }
 }
--- Assets/Source/Settings/GameSettings.cs
+++ Assets/Source/Settings/GameSettings.cs
@@ -12,9 +12,10 @@ namespace RuthGoldbergGame
 
         }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
