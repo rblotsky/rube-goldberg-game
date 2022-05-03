b2acd87e09d511acd05724a5679b6cf7877a2838
rblotsky <roman.blotsky@gmail.com>
2022-05-02 20:32:16 -0400

    Started sample scene

---
+++ Assets/Source/Game Levels/DestinationRegion.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RuthGoldbergGame
+{
+    public class DestinationRegion : BlockBase
+    {
+        
+    }
+}
