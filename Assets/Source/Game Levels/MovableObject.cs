de0617f29767eecb42a7e9554c99318033ffb38c
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2022-05-05 17:03:14 -0400

    implemented tooltips

--- Assets/Source/Game Levels/MovableObject.cs
+++ Assets/Source/Game Levels/MovableObject.cs
@@ -9,5 +9,20 @@ namespace RubeGoldbergGame
         // DATA //
         // Toggles
         public bool isObjectiveObject = false;
+        public string displayName;
+        public string displayDescription;
+        public Sprite displaySprite;
+        
+        private void OnMouseEnter()
+        {
+            //Debug.Log("I am hovered over!");
+            FindObjectOfType<LevelUIManager>().ToggleTooltipUI(displayName +": "+ displayDescription); //TODO replace this with a better method
+        }
+
+        private void OnMouseExit()
+        {
+            //Debug.Log("I am not hovered over anymore");
+            FindObjectOfType<LevelUIManager>().ToggleTooltipUI("");
+        }
     }
 }
