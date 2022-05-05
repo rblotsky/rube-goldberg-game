6fcdc7ffb6c868c2644e8a55f9a111546e540cf5
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2022-05-05 10:57:39 -0400

    added base functionality for a reactive object

--- Assets/Source/Game Levels/ReactiveObject.cs
+++ Assets/Source/Game Levels/ReactiveObject.cs
@@ -1,9 +1,18 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace RubeGoldbergGame
 {
-    public class ActiveObject : BlockBase
+    public class ReactiveObject : BlockBase
     {
+        //this is a test object
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            TriggerBlockFunctionality();
+        }
+
         public new void TriggerBlockFunctionality()
         {
             Debug.Log("This should do something to anything that goes in my trigger zone");
