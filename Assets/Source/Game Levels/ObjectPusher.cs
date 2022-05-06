c94bd84c0e127b685dc2eedad6bfcfac1e33a9a7
rblotsky <roman.blotsky@gmail.com>
2022-05-06 09:06:01 -0400

    Comments + removed member hiding in ObjectPusher

--- Assets/Source/Game Levels/ObjectPusher.cs
+++ Assets/Source/Game Levels/ObjectPusher.cs
@@ -6,28 +6,34 @@ using UnityEngine.EventSystems;
 
 namespace RubeGoldbergGame
 {
-    public class ObjectPusher : BlockBase, IPointerEnterHandler, IPointerExitHandler
+    public class ObjectPusher : BlockBase
     {
-        //this is a test object
+        // DATA //
         public BoxCollider2D triggerArea;
         private LevelUIManager interfaceManager;
-        private bool isUserHovering = false;
         
+
+        // FUNCTIONS //
+        // Unity Defaults
         private void Awake()
         {
             interfaceManager = FindObjectOfType<LevelUIManager>();
         }
+
         private void OnTriggerStay2D(Collider2D col)
         {
             TriggerBlockFunctionality(col);
         }
         
+
+        // Block Functionality Functions
         //triggerArea collider will only be active during simulation runtime
         //allows tooltips to work only on the object
         public override void ToggleTriggerArea(bool inSimMode)
         {
             triggerArea.enabled = inSimMode;
         }
+        
         public new void TriggerBlockFunctionality()
         {
             Debug.Log("This should do something to anything that goes in my trigger zone");
@@ -36,24 +42,6 @@ namespace RubeGoldbergGame
         public void TriggerBlockFunctionality(Collider2D col)
         {
             col.attachedRigidbody.velocity += Vector2.right * gameObject.transform.right;
-        }
-        
-        private void LateUpdate()
-        {
-            if(isUserHovering)
-            {
-                interfaceManager.OpenTooltipUI(displayName + "\n\n" + displayDescription, Input.mousePosition);
-            }
-        }
-        public void OnPointerEnter(PointerEventData pointerData)
-        {
-            isUserHovering = true;
-        }
-
-        public void OnPointerExit(PointerEventData pointerData)
-        {
-            interfaceManager.CloseTooltipUI();
-            isUserHovering = false;
         }
     }
 }
