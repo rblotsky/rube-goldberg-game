8ff85ebf6b246e62bc0c73c020630a6284c3b657
rblotsky <roman.blotsky@gmail.com>
2022-05-22 16:20:01 -0400

    completionregions have a required movable object

--- Assets/Source/Game Levels/Blocks/LevelCompletionRegion.cs
+++ Assets/Source/Game Levels/Blocks/LevelCompletionRegion.cs
@@ -2,12 +2,17 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 namespace RubeGoldbergGame
 {
     public class LevelCompletionRegion : MonoBehaviour
     {
         // DATA //
+        // References
+        public MovableObject requiredMovableObject;
+        public TextMeshProUGUI numberRequirementText;
+
         // Usage Data
         public Completion completionUsed;
 
@@ -19,12 +24,29 @@ namespace RubeGoldbergGame
         // Unity Defaults
         private void Awake()
         {
+            // Finds references
             levelManager = FindObjectOfType<LevelManager>();
+            numberRequirementText = GetComponentInChildren<TextMeshProUGUI>();
+
+            // If there is a required object and number text, sets it to show the name of the object that has to go there
+            if(requiredMovableObject != null && numberRequirementText != null)
+            {
+                numberRequirementText.SetText(requiredMovableObject.name);
+            }
         }
 
         private void OnTriggerEnter2D(Collider2D col)
         {
-            levelManager.CompleteLevel(completionUsed);
+            // If the collided object is the required one, completes the level
+            MovableObject collidedObject = col.GetComponent<MovableObject>();
+            if(collidedObject != null)
+            {
+                // If it's the required one, or there is no required one, runs completelevel with the completion given
+                if (collidedObject == requiredMovableObject || requiredMovableObject == null)
+                {
+                    levelManager.CompleteLevel(completionUsed);
+                }
+            }
         }
     }
 }
