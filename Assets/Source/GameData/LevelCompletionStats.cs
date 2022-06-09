08ecfbfbe47ef88aee67e15ce4fd6387bca937e5
rblotsky <roman.blotsky@gmail.com>
2022-06-09 19:46:13 -0400

    Started level stats

---
+++ Assets/Source/GameData/LevelCompletionStats.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCompletionStats
+{
+    // DATA //
+    // Statistics (Private)
+
+
+    // Properties (Public accessors for statistics)
+    
+
+    // CONSTRUCTOR //
+
+
+    // FUNCTIONS //
+
+}
