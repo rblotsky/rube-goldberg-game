fdeaea0831acfb703bfb04b59e1298a0da0295a4
rblotsky <roman.blotsky@gmail.com>
2022-06-12 10:40:23 -0400

    Added an audio mixer and worked on level stats

--- Assets/Source/GameData/LevelCompletionStats.cs
+++ Assets/Source/GameData/LevelCompletionStats.cs
@@ -2,18 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelCompletionStats
+namespace RubeGoldbergGame
 {
-    // DATA //
-    // Statistics (Private)
+    public class LevelCompletionStats
+    {
+        // DATA //
+        // Statistics (Private)
+        private Completion levelCompletion;
+        private int bestBlocksUsed = -1;
+        private float bestTime = -1;
+        private bool[] optionalsCompleted = { false, false, false };
+        private float[] bestTimeWithOptionalAmounts = { -1, -1, -1 };
+        private int[] bestBlocksWithOptionalAmounts = { -1, -1, -1 };
+
+        // Properties (Public accessors for statistics)
+        public int MainObjBestBlocks { get { return bestBlocksUsed; } }
+        public float MainObjBestTime { get { return bestTime; } }
+        public bool[] CompletedOptionals { get { return optionalsCompleted; } }
+        public float[] OptionalAmountBestTimes { get { return bestTimeWithOptionalAmounts; } }
+        public int[] OptionalAmountBestBlocks { get { return bestBlocksWithOptionalAmounts; } }
 
 
-    // Properties (Public accessors for statistics)
+        // CONSTRUCTOR //
+        //TODO
+
+
+        // FUNCTIONS //
+        // Managing Bests
+        public void UpdateOptionalBests(int numOptionals, int blocksUsed, float timeTaken)
+        {
+            //TODO
+        }
+
+        public void UpdateLevelStats(bool isComplete)
+        {
+            //TODO
+            // Consider merging this with UpdateOptionalBests b/c 0 optionals is a valid amount of optionals to complete
+        }
     
-
-    // CONSTRUCTOR //
-
-
-    // FUNCTIONS //
-
+}
 }
