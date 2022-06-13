d4855d2e17f50034d2df5a263ac47f55bc4141de
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2022-06-12 22:12:48 -0400

    cleaned up script

--- Assets/Source/Game Levels/Blocks/ObjectSpringPlate.cs
+++ Assets/Source/Game Levels/Blocks/ObjectSpringPlate.cs
@@ -6,11 +6,10 @@ namespace RubeGoldbergGame
     public class ObjectSpringPlate : MonoBehaviour
     {
         // DATA //
-        public float pushForce = 2; //min/max are 0.1,40?
-        public float pushTime = 0.2f;
+        public float pushForce = 20; //min/max are 0.1,40?
         public float cooldownTime = 1.0f;
         private float activeTime = 0f;
-        private bool isPushing = false;
+        private bool onCooldown = false;
         private Rigidbody2D attachedRigidbody;
 
 
@@ -24,17 +23,13 @@ namespace RubeGoldbergGame
 
         private void Update()
         {
-            if (isPushing)
+            if (onCooldown)
             {
                 //active time above 0 is pushing, active time below 0 is cooldown phase
                 activeTime -= Time.unscaledDeltaTime;
-                if (activeTime > 0)
+                if (activeTime < 0)
                 {
-                    //attachedRigidbody.AddForce(transform.up * pushForce, ForceMode2D.Impulse);
-                }
-                else if (activeTime < -cooldownTime) 
-                {
-                    isPushing = false;
+                    onCooldown = false;
                 }
             }
         }
@@ -42,17 +37,15 @@ namespace RubeGoldbergGame
         
         //trigger function
         //starts the spring pushing process
-        private void OnCollisionEnter2D(Collision2D collision)
+        private void OnCollisionStay2D(Collision2D collision)
         {
-            Debug.Log("Collision: " + collision.collider.name);
+            Debug.Log("Spring Collision: " + collision.collider.name);
             // If the collided object has a rigidbody and the process is not started yet
-            if (collision.collider.attachedRigidbody != null && !isPushing)
+            if (collision.collider.attachedRigidbody != null && !onCooldown)
             {
-                isPushing = true;
+                onCooldown = true;
                 attachedRigidbody.AddForce(transform.up * pushForce, ForceMode2D.Impulse);
-                activeTime = pushTime;
-                Debug.Log("Adding force to: " + collision.collider.name);
-                
+                activeTime = cooldownTime;
             }
         }
     }
