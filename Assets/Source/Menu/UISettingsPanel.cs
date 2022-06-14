919484a53b884747f71ddaa8d58fea5dd1bde54d
rblotsky <roman.blotsky@gmail.com>
2022-06-14 19:24:03 -0400

    Settings now saved to PlayerPrefs

--- Assets/Source/Menu/UISettingsPanel.cs
+++ Assets/Source/Menu/UISettingsPanel.cs
@@ -12,9 +12,16 @@ namespace RubeGoldbergGame
         // DATA //
         // Scene References
         public TMP_Dropdown resolutionsDropdown;
+        public TMP_Dropdown graphicsQualityDropdown;
+        public Toggle fullscreenToggle;
 
         // Cached data
         private Resolution[] screenResolutions;
+
+        // Constants
+        public static readonly string GRAPHICS_QUALITY_KEY = "GraphicsQuality";
+        public static readonly string FULLSCREEN_KEY = "IsFullscreen";
+        public static readonly string SCREEN_RES_KEY = "ScreenResolution";
 
 
         // FUNCTIONS //
@@ -46,19 +53,52 @@ namespace RubeGoldbergGame
         }
 
 
+        // Setup
+        public void LoadSettingsFromPlayerPrefs()
+        {
+            // Gets data from PlayerPrefs
+            bool isFullscreen = PlayerPrefs.GetInt(FULLSCREEN_KEY, 0) == 1;
+            int graphicsQualityIndex = PlayerPrefs.GetInt(GRAPHICS_QUALITY_KEY, 0);
+            int resolutionsIndex = PlayerPrefs.GetInt(SCREEN_RES_KEY, 0);
+
+            // Updates UI (and by consequence, events run, and the actual settings are modified)
+            fullscreenToggle.isOn = isFullscreen;
+            graphicsQualityDropdown.value = graphicsQualityIndex;
+            resolutionsDropdown.value = resolutionsIndex;
+        }
+
+
         // UI Events
         public void OnGraphicsQualityChange(int qualityIndex)
         {
+            // Saves to PlayerPrefs
+            PlayerPrefs.SetInt(GRAPHICS_QUALITY_KEY, qualityIndex);
+
+            // Modifies data
             QualitySettings.SetQualityLevel(qualityIndex);
         }
 
         public void SetFullscreen(bool isFullscreen)
         {
+            // Saves to PlayerPrefs
+            if (isFullscreen)
+            {
+                PlayerPrefs.SetInt(FULLSCREEN_KEY, 1);
+            }
+            else
+            {
+                PlayerPrefs.SetInt(FULLSCREEN_KEY, 0);
+            }
+
+            // Modifies data
             Screen.fullScreen = isFullscreen;
         }
 
         public void SetResolution(int resolutionIndex)
         {
+            // Saves to PlayerPrefs
+            PlayerPrefs.SetInt(SCREEN_RES_KEY, resolutionIndex);
+
             // Sets the resolution to the one at the current selected index, fullscreen stays the same as it currently is
             Screen.SetResolution(screenResolutions[resolutionIndex].width, screenResolutions[resolutionIndex].height, Screen.fullScreen);
         }
