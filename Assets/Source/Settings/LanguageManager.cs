cc2092c08127b37b6ecfe3cc49c38fbfc364a13b
rblotsky <roman.blotsky@gmail.com>
2022-06-14 19:41:45 -0400

    Added base scripts for future language selection

---
+++ Assets/Source/Settings/LanguageManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RubeGoldbergGame
+{
+    public class LanguageManager
+    {
+        // DATA //
+        //TODO
+
+
+
+        // FUNCTIONS //
+        // Translating Functions
+        public string TranslateFromEnglish(string englishText)
+        {
+            //TODO
+            return englishText;
+        }
+
+
+        // Data Loading
+        //TODO: Decide if this needs to be saved as a file or a database. Maybe database, but we might have a small enough amount of text that a csv file would work.
+    }
+}
