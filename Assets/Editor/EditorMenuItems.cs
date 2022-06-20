7c60aee90a50e8c27992856be06dd335b6e7eab5
rblotsky <roman.blotsky@gmail.com>
2022-06-20 17:16:46 -0400

    Updated EditorMenuItems to work properly

--- Assets/Editor/EditorMenuItems.cs
+++ Assets/Editor/EditorMenuItems.cs
@@ -3,20 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.IO;
 
-public class EditorMenuItems
+namespace RubeGoldbergGame
 {
-    // FUNCTIONS //
-    [MenuItem("Tools/Set Sprites to 80 Pixels Per Unit")]
-    public void UpdateSpriteSettings()
+    public static class EditorMenuItems
     {
-        // Gets all sprites
-        string[] files = AssetDatabase.FindAssets("t:Sprite");
-
-        foreach(string filePath in files)
-        {
-        }
-
 
     }
 }
