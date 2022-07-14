4e7737c899b58769bc8499a06eb61ff3169c1103
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2022-07-14 19:51:22 -0400

    adjusted the movement to work on grid

--- Assets/Source/Game Levels/ManagerMoveSelection.cs
+++ Assets/Source/Game Levels/ManagerMoveSelection.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 namespace RubeGoldbergGame
 {
-    public class SelectionMove : MonoBehaviour
+    public class ManagerMoveSelection : MonoBehaviour
     {
         public ManagerSelectingBase baseSel;
         private Camera mainCam;
+
+        private Grid placementGrid;
         public List<Vector3> mouseOffsets = new List<Vector3>();
+        private List<Vector3> oldPositions = new List<Vector3>();
         private void Start()
         {
             mainCam = Camera.main;
             this.enabled = false;
+            placementGrid = FindObjectOfType<Grid>(true);
         }
 
         public void OnEnable()
@@ -20,19 +24,43 @@ namespace RubeGoldbergGame
             baseSel = ManagerSelectingBase.SelectingManagerInstance;
             foreach (var obj in baseSel.selectedObjects)
             {
-                
-                mouseOffsets.Add(obj.transform.position - Vector3.Scale(mainCam.ScreenToWorldPoint(Input.mousePosition),
+                Vector3 position = obj.transform.position;
+                mouseOffsets.Add(position - Vector3.Scale(mainCam.ScreenToWorldPoint(Input.mousePosition),
                     (new Vector3(1, 1, 0))));
+                oldPositions.Add(position);
             }
         }
 
         private void Update()
         {
+            Vector3 mouseCoords =
+                Vector3.Scale(mainCam.ScreenToWorldPoint(Input.mousePosition), (new Vector3(1, 1, 0)));
+
+            bool canMoveBlocks = true;
+            
+            //moving each block
             for (int i = 0; i < baseSel.selectedObjects.Count; i++)
             {
-                Vector3 mouseCoords =
-                    Vector3.Scale(mainCam.ScreenToWorldPoint(Input.mousePosition), (new Vector3(1, 1, 0)));
-                baseSel.selectedObjects[i].transform.position = mouseCoords + mouseOffsets[i];
+                //record old positions
+                oldPositions[i] = baseSel.selectedObjects[i].transform.position;
+                
+                //move each block to new pos, record when we cannot move the block
+                if(!baseSel.selectedObjects[i].GetComponent<BlockBase>().RunBlockMove(
+                       UtilityFuncs.SnapToGrid(mouseCoords + mouseOffsets[i], placementGrid)))
+                {
+                    canMoveBlocks = false;
+                }
+                
+                //baseSel.selectedObjects[i].transform.position = mouseCoords + mouseOffsets[i];
+            }
+            
+            //move all blocks back to old position if we cannot move
+            if (!canMoveBlocks)
+            {
+                for (int i = 0; i < baseSel.selectedObjects.Count; i++)
+                {
+                    baseSel.selectedObjects[i].transform.position = oldPositions[i];
+                }
             }
 
             if (Input.GetMouseButtonUp(0))
