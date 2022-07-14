bd9b05ef7a81cfa53822e9171bd5c929cd6b06aa
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2022-07-14 09:17:39 -0400

    added move script

---
+++ Assets/Source/Game Levels/ManagerMoveSelection.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RubeGoldbergGame
+{
+    public class SelectionMove : MonoBehaviour
+    {
+        public ManagerSelectingBase baseSel;
+        private Camera mainCam;
+        public List<Vector3> mouseOffsets = new List<Vector3>();
+        private void Start()
+        {
+            mainCam = Camera.main;
+            this.enabled = false;
+        }
+
+        public void OnEnable()
+        {
+            baseSel = ManagerSelectingBase.SelectingManagerInstance;
+            foreach (var obj in baseSel.selectedObjects)
+            {
+                
+                mouseOffsets.Add(obj.transform.position - Vector3.Scale(mainCam.ScreenToWorldPoint(Input.mousePosition),
+                    (new Vector3(1, 1, 0))));
+            }
+        }
+
+        private void Update()
+        {
+            for (int i = 0; i < baseSel.selectedObjects.Count; i++)
+            {
+                Vector3 mouseCoords =
+                    Vector3.Scale(mainCam.ScreenToWorldPoint(Input.mousePosition), (new Vector3(1, 1, 0)));
+                baseSel.selectedObjects[i].transform.position = mouseCoords + mouseOffsets[i];
+            }
+
+            if (Input.GetMouseButtonUp(0))
+            {
+                mouseOffsets.Clear();
+                this.enabled = false;
+            }
+        }
+    }
+}
