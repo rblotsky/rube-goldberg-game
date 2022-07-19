e34b9d336a15637b330e9c43b92b635cdce261cd
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2022-07-18 21:17:32 -0400

    added new script background selection

---
+++ Assets/Source/Game Levels/BackgroundHitbox.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace RubeGoldbergGame
+{
+    public class BackgroundHitbox : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        private bool isHovering = false;
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            isHovering = true;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            isHovering = false;
+        }
+
+        public void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (isHovering)
+                {
+                    ManagerSelectingBase.SelectingManagerInstance.ObjectClickedOn(gameObject);
+                }
+            }
+        }
+    }
+}
