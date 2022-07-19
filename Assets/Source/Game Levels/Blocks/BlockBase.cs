3e34d03eec2b32b53b44a89f55a03f39bb6d7864
Daniel Wang <48870892+DanWang725@users.noreply.github.com>
2022-07-18 21:17:19 -0400

    removed function

--- Assets/Source/Game Levels/Blocks/BlockBase.cs
+++ Assets/Source/Game Levels/Blocks/BlockBase.cs
@@ -100,6 +100,7 @@ namespace RubeGoldbergGame
             objectSelectionManager = GetComponent<ObjectSelectionBase>();
             objectCollider = GetComponent<BoxCollider2D>();
             thisSpriteRenderer = GetComponent<SpriteRenderer>();
+            ManagerSelectingBase.SelectingManagerInstance.AddNewObjectToGlobalList(gameObject);
 
             // Updates its default positions on awake
             UpdateOriginalTransform();
@@ -122,11 +123,6 @@ namespace RubeGoldbergGame
 
             // Returns true if it was able to be moved
             return true;
-        }
-
-        public void SaveMouseOffset(Vector2 snappedWorldMousePos)
-        {
-            dragMouseOffset = (Vector2)transform.position - snappedWorldMousePos;
         }
 
 
@@ -255,23 +251,6 @@ namespace RubeGoldbergGame
         }
 
         
-        // Confirmation Function
-        public void ClickedOn()
-        {
-            try
-            {
-                objectSelectionManager.durationSelected = 0f;
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e + ": the object shouldn't be clicked!");
-                
-                throw;
-            }
-            
-            isClickedOn = true;
-        }
-
 
         // Simulation Management
         public void SimulationResetPos()
